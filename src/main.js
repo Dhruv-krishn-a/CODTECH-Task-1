52d56acdbb084adf1f916367305a3321053f5e4b
Dhruv-krishn-a <dhruvkrishna1055@gmail.com>
2024-07-04 16:41:01 +0530

    added ABOUT section .

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import '../styles/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/component/header.css'
 import '../styles/component/hero.css'
+import '../styles/component/about.css'
 import '../styles/utils.css';
