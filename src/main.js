2a8dcb99f70f0f90b7d4d728bed80210c83291e1
Dhruv-krishn-a <dhruvkrishna1055@gmail.com>
2024-07-04 19:56:13 +0530

    Added Featured Projects and timeline with screenshot .

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import '../styles/style.css';
 import '../styles/component/header.css'
 import '../styles/component/hero.css'
 import '../styles/component/about.css'
+import '../styles/component/featured.css'
 import '../styles/utils.css';
