445c037893a4d35a1f1846c96837866e048c299f
Dhruv-krishn-a <dhruvkrishna1055@gmail.com>
2024-07-04 15:17:27 +0530

    Created a MAIN Section for my Website.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
-
+import '../styles/component/header.css'
+import '../styles/component/hero.css'
 import '../styles/utils.css';
